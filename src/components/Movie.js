be781be6229d7568dc4d1355d51a4bf83ccaff37
jorenchik <jorens.stekels@gmail.com>
2021-08-01 16:53:57 +0300

    Actors list

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,20 +2,21 @@ import React from "react";
 import { useParams } from "react-router";
 
 // Config
-// import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
 
 // Components
-import BreadCrumb from "./BreadCrumb";
-import MovieInfoBar from "./MovieInfoBar";
-// import Grid from "./Grid";
 import Spinner from "./Spinner";
+import BreadCrumb from "./BreadCrumb";
 import MovieInfo from "./MovieInfo";
+import Grid from "./Grid";
+import MovieInfoBar from "./MovieInfoBar";
+import Actor from "./Actor";
 
 // Hook
 import { useMovieFetch } from "../hooks/useMovieFetch";
 
 // Image
-// import NoImage from "../images/no_image.jpg";
+import NoImage from "../images/no_image.jpg";
 
 const Movie = () => {
   const { movieId } = useParams();
@@ -34,6 +35,20 @@ const Movie = () => {
         budget={movie.budget}
         revenue={movie.revenue}
       />
+      <Grid header="Actors">
+        {movie.actors.map((actor) => (
+          <Actor
+            key={actor.credit_id}
+            name={actor.name}
+            character={actor.character}
+            imageUrl={
+              actor.profile_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                : NoImage
+            }
+          />
+        ))}
+      </Grid>
     </>
   );
 };
